e5f8142ba610f33b02116f48452dda90d8dc8786
robertoleitonesquivel <rleiton22@gmail.com>
2023-12-08 21:55:01 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto2_SaikuGames/Controllers/JuegosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto2_SaikuGames.Controllers
+{
+    public class JuegosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proyecto2_SaikuGames/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto2_SaikuGames.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
